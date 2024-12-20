bde01c52880b6ab242411575af7d38749907b1ba
Mimi Zhang <MimiZhang2027@u.northwestern.edu>
2024-12-20 12:55:11 -0500

    fixed payments page images

--- src/components/PaymentPage/PaymentPage.tsx
+++ src/components/PaymentPage/PaymentPage.tsx
@@ -40,6 +40,7 @@ const PaymentPage = ({ language }: Props) => {
         text={data?.image_block.text}
         color={data?.image_block.color}
       />
+      {/* Payment 1*/}
       <div style={{ margin: "5% 5% 5% 5%" }}>
         <div className="accordion custom-accordian" id="accordionExample">
           <div className="accordion-item">
@@ -68,57 +69,69 @@ const PaymentPage = ({ language }: Props) => {
               </div>
             </div>
           </div>
-          {data?.payment_options.map(
-            (payment: any) =>
-              payment.id > 0 && (
-                <div className="accordion-item">
-                  <h2 className="accordion-header">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target={"#collapse" + payment.id}
-                      aria-expanded="true"
-                      aria-controls={"collapse" + payment.id}
-                    >
-                      {payment.payment_method}
-                    </button>
-                  </h2>
-                  <div
-                    id={"collapse" + payment.id}
-                    className="accordion-collapse collapse"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div style={{ margin: "3%" }}>
-                      <FormattedText
-                        content={payment.content}
-                        content_id={payment.id}
-                      />
-                      <br></br>
-                      <a href="https://secure.lawpay.com/pages/mtlawllc/trust">
-                        <img
-                          style={{ border: "solid 1px black" }}
-                          src="/public/assets/PaymentPage/LawPayButton.png"
-                        ></img>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              )
-          )}
+          {/* Payment 2*/}
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target={"#collapse" + data?.payment_options[1].id}
+                aria-expanded="true"
+                aria-controls={"collapse" + data?.payment_options[1].id}
+              >
+                {data?.payment_options[1].payment_method}
+              </button>
+            </h2>
+            <div
+              id={"collapse" + data?.payment_options[1].id}
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div style={{ margin: "3%" }}>
+                <FormattedText
+                  content={data?.payment_options[1].content}
+                  content_id={data?.payment_options[1].id}
+                />
+              </div>
+            </div>
+          </div>
+          {/* Payment 3*/}
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target={"#collapse" + data?.payment_options[2].id}
+                aria-expanded="true"
+                aria-controls={"collapse" + data?.payment_options[2].id}
+              >
+                {data?.payment_options[2].payment_method}
+              </button>
+            </h2>
+            <div
+              id={"collapse" + data?.payment_options[2].id}
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div style={{ margin: "3%" }}>
+                <FormattedText
+                  content={data?.payment_options[2].content}
+                  content_id={data?.payment_options[2].id}
+                />
+                <br></br>
+                <a href="https://secure.lawpay.com/pages/mtlawllc/trust">
+                  <img
+                    style={{ border: "solid 1px black" }}
+                    src="/assets/PaymentPage/LawPayButton.png"
+                  ></img>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      {/* <div style={{ display: "grid" }}>
-        {data?.payment_options.map((payment: any) => (
-          <div className={"payment-card-" + payment.side}>
-            <h1 style={{ color: "var(--primary-color)" }}>
-              {payment.payment_method}
-            </h1>
-            <FormattedText content={payment.content} content_id={payment.id} />
-          </div>
-        ))}
-      </div> */}
     </div>
   );
 };
